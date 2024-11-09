ca02751d5e061a391019dbe1dc53b8cce2ce4904
Fernando <Fernando@DESKTOP-JHGBRM4>
2024-11-09 15:17:28 -0600

    Frame

--- src/POO/Herencia.java
+++ src/POO/Herencia.java
@@ -1,6 +1,9 @@
 
 package POO;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author Fernando Lopez
@@ -12,5 +15,14 @@ public class Herencia {
         Animal p = new Perro("Negro", "SI", "SI", "SI", 4); p.Sonido();
         Animal g = new Gato("Blanco", "SI", "SI", "SI", 4); g.Sonido();
     }
-
+    
+    List<Animal> lista = new ArrayList<>();
+        lista.add(p);
+        lista.add(g);
+        lista.add(new Queco("NO", "SI", "SI", "SI", 4));
+    
+    for (Animal item : lista){
+        item.Sonido();
+    }
+    
 }
